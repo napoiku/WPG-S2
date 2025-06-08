22a003eb22f74d9c50fa586d3c95dc2ac4aca148
HafizAFauzi <139345624+HafizAFauzi@users.noreply.github.com>
2025-06-08 08:48:35 +0700

    fix spawner

--- WPG-S2/Assets/Script/enemy/enemySpawner.cs
+++ WPG-S2/Assets/Script/enemy/enemySpawner.cs
@@ -2,47 +2,65 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class enemySpawner : MonoBehaviour
+public class EnemySpawner : MonoBehaviour
 {
     [SerializeField]
     private GameObject enemyPrefab;
 
     [SerializeField]
-    private float minimumSpawnTime;
+    private float minimumSpawnTime = 1f;
 
     [SerializeField]
-    private float maximumSpawnTime;
+    private float maximumSpawnTime = 3f;
+
+    private Collider2D mapCollider;
     private float timeUntilSpawn;
 
-    // Start is called before the first frame update
     void Awake()
     {
-        setTimeUntilSpawn();
+        // Cari objek dengan tag "MapArea"
+        GameObject mapObj = GameObject.FindGameObjectWithTag("Map Area");
+
+        if (mapObj != null)
+        {
+            mapCollider = mapObj.GetComponent<Collider2D>();
+            if (mapCollider == null)
+            {
+                Debug.LogError("Map ditemukan tapi tidak ada Collider2D-nya!");
+            }
+        }
+        else
+        {
+            Debug.LogError("Tidak ditemukan GameObject dengan tag 'Map Area'");
+        }
+
+        SetTimeUntilSpawn();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (mapCollider == null) return;
+
         timeUntilSpawn -= Time.deltaTime;
 
-        if (timeUntilSpawn <= 0) 
+        if (timeUntilSpawn <= 0f)
         {
-            Vector2 spawnPosition = GetRandomSpawnPosition();
+            Vector2 spawnPosition = GetRandomSpawnPositionInMap();
             Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
-            setTimeUntilSpawn();
-        }        
+            SetTimeUntilSpawn();
+        }
     }
 
-    private Vector2 GetRandomSpawnPosition()
+    private Vector2 GetRandomSpawnPositionInMap()
     {
-        float randomX = Random.Range(-240f, 240f); 
-        float randomY = Random.Range(-240f, 240f); 
+        Bounds bounds = mapCollider.bounds;
+        float randomX = Random.Range(bounds.min.x, bounds.max.x);
+        float randomY = Random.Range(bounds.min.y, bounds.max.y);
         return new Vector2(randomX, randomY);
     }
 
-    private void setTimeUntilSpawn()
+    private void SetTimeUntilSpawn()
     {
         timeUntilSpawn = Random.Range(minimumSpawnTime, maximumSpawnTime);
-
     }
 }
